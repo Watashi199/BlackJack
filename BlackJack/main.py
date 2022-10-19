5b447d59d64ec55d1cf558f88c66bf7b1ee880dc
Watashi <drazy24@gmail.com>
2022-10-19 20:37:23 +0200

    concatenation des fonctions

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -7,56 +7,26 @@ main_joueur = []
 #Carte en main du croupier
 main_croupier = []
 
+joueur = "Joueur"
+croupier = "Croupier"
+
 #Fonction pour faire la somme de la main du joueur
-def somme_cartes_joueur():
-    valeur_joueur = 0
+def somme_cartes(main, qui):
+    valeur_main = 0
     #Boucle pour additionner le résultat
-    for i in range(len(main_joueur)):
-        valeur_joueur += main_joueur[i]
+    for i in range(len(main)):
+        valeur_main += main[i]
     #Si la main vaut plus de 21 c'est perdu gros noob
-    if valeur_joueur > 21:
+    if valeur_main > 21:
+        print(valeur_main)
         print("Perdu tu as dépassé 21")
         exit()
-    elif valeur_joueur == 21:
-        print(main_joueur)
-        print("BLACKJACK")
+    elif valeur_main == 21:
+        print(main)
+        print(qui,"a fait BLACKJACK")
         exit()
-        
-def somme_cartes_croupier():
-    valeur_croupier = 0
-    #Boucle pour additionner le résultat
-    for i in range(len(main_croupier)):
-        valeur_croupier += main_croupier[i]
-    #Si la main vaut plus de 21 c'est perdu gros noob
-    if valeur_croupier > 21:
-        print("Gagné le croupier a dépassé 21")
-        exit()
-    elif valeur_croupier == 21:
-        print(main_croupier)
-        print("Le croupier gagne !")
-        exit()
-
-#Fonction pour distribuer une carte au joueur
-def tirer_carte_joueur():
-    carte_tire = random.choice(paquet)
-    #Transformation des strings en int pour additionner
-    match carte_tire:
-        case "J":
-            carte_tire = 10
-        case "Q":
-            carte_tire = 10
-        case "K":
-            carte_tire = 10
-        case "A":
-            carte_tire = 11
-        case _:
-            carte_tire = int(carte_tire)
-    #Ajout de la carte  a la main du joueur 
-    main_joueur.append(carte_tire)
-    somme_cartes_joueur()
-    
-#Fonction pour distribuer une carte au croupier    
-def tirer_carte_croupier():
+   
+def tirer_carte(main,qui):
     carte_tire = random.choice(paquet)
     #Transformation des strings en int pour additionner
     match carte_tire:
@@ -71,31 +41,30 @@ def tirer_carte_croupier():
         case _:
             carte_tire = int(carte_tire)
     #Ajout de la carte  a la main du croupier 
-    main_croupier.append(carte_tire)
-    somme_cartes_croupier()
+    main.append(carte_tire)
+    somme_cartes(main,qui)
 
 def choix():
     print("Que veux tu faire ?")
-    if somme_cartes_joueur == 21:
+    if somme_cartes(main_joueur, joueur) == 21:
         print("Ta main",main_joueur[0],",", main_joueur[1])
         print("BLACKJACK")
         exit()
     elif input() == "Tirer":
-        tirer_carte_joueur()
+        tirer_carte(main_joueur, joueur)
         print("Ta main",main_joueur[0],",", main_joueur[1],",",main_joueur[2])
     elif input() == "FF":
+        print("Wallah j'abandonne")
         exit()
     elif input() == "Passe":
         return
-    else:
-        choix()
 
 #Fonction pour démarrer la partie
 def debut_partie():
-    tirer_carte_joueur()
-    tirer_carte_joueur()
+    tirer_carte(main_joueur, joueur)
+    tirer_carte(main_joueur, joueur)
     print("Ta main",main_joueur[0],",", main_joueur[1])
-    tirer_carte_croupier()
+    tirer_carte(main_croupier, joueur)
     print("Main du croupier",main_croupier[0])
     choix()
 
